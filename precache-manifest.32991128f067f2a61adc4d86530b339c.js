f74828231b28170b122aefd3fd8437b7d45e81fa
Dharmendra Singh <dharmendra.kp12@gmail.com>
2022-11-02 20:39:17 +0530

    Updates

---
+++ precache-manifest.32991128f067f2a61adc4d86530b339c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5773056203a97a49fa5b2c630f8ed40",
+    "url": "/keeper2/index.html"
+  },
+  {
+    "revision": "59ba15f119b08b666524",
+    "url": "/keeper2/static/js/2.ff454e0b.chunk.js"
+  },
+  {
+    "revision": "9c740ed0ee1192b13ae2",
+    "url": "/keeper2/static/js/main.90c784d4.chunk.js"
+  },
+  {
+    "revision": "4be8648e9db0c1ed8971",
+    "url": "/keeper2/static/js/runtime-main.421d213f.js"
+  }
+]);
