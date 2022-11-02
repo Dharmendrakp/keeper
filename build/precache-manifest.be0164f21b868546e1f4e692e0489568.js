07ba8d9938e3eecf9dc170fac94816ad8984ff2c
Dharmendra Singh <dharmendra.kp12@gmail.com>
2022-11-02 20:38:25 +0530

    deploy my react app

---
+++ build/precache-manifest.be0164f21b868546e1f4e692e0489568.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f0ef4f1b2379f822e9624b6463625c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ba15f119b08b666524",
+    "url": "/static/js/2.ff454e0b.chunk.js"
+  },
+  {
+    "revision": "9c740ed0ee1192b13ae2",
+    "url": "/static/js/main.90c784d4.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
